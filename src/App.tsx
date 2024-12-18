e2fa6c2905b9e1371cd2b900ab5d28c152f0c651
Anjali-Vipparthi <anjiianjali2004@gmail.com>
2024-12-18 15:04:45 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MyFirstcom from './components/MyFirstcom';
+
+
+export default function App() {
+  return (
+    <div>
+      <MyFirstcom></MyFirstcom>
+    </div>
+  );
+}
